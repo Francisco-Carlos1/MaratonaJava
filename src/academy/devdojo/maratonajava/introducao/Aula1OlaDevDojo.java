2f7cf33144cdc9754f6da0e3bf6c1a679ea9a096
Francisco1 <franciscocarlosmateus@yahoo.com.br>
2025-07-10 20:14:11 -0300

    First Commit Maratona Java DevDojo

---
+++ src/academy/devdojo/maratonajava/introducao/Aula1OlaDevDojo.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * isso é um comentario <b>javadoc</b>
+ *
+ */
+
+public class Aula1OlaDevDojo {
+    public static void main (String[] args) {
+            System.out.println("KA-ME-HA-ME-HA");
+    }
+}
+//comentario de um linha
+/*
+comentario
+de
+multiplas linhas
+ */
+
